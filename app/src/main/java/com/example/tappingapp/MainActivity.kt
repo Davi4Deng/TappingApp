d31cbc8e5f28f9aa0a640911b228814bdee9f71a
Li <yl935@njit.edu>
2024-09-10 21:29:00 -0400

    Initial commit

---
+++ app/src/main/java/com/example/tappingapp/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.tappingapp
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        var i = 0
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+
+            // Corrected spelling and removed unnecessary parentheses
+            findViewById<Button>(R.id.b1).setOnClickListener {
+                i++
+                findViewById<TextView>(R.id.textView).text = "Tapped: $i"
+            }
+            insets
+        }
+    }
+}
